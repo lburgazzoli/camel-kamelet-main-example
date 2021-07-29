0f4d23ba33395ec824a604a9d02872412e254721
Luca Burgazzoli <lburgazzoli@gmail.com>
2021-07-29 14:26:57 +0200

    update

---
+++ src/main/java/com/github/lburgazzoli/camel/Application.java
@@ -0,0 +1,10 @@
+package com.github.lburgazzoli.camel;
+
+import org.apache.camel.main.Main;
+
+public class Application {
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
+        main.run(args);
+    }
+}
